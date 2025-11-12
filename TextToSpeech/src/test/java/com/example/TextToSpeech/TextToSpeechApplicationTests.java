22e54d252a7d48b7895570a65dc303f8e99afd92
leraner23 <killernknwn8@gmail.com>
2025-11-12 13:24:33 +0545

    commit

---
+++ TextToSpeech/src/test/java/com/example/TextToSpeech/TextToSpeechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TextToSpeech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextToSpeechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
