dba88d38da4ed95f33d80ecbd9d847071a7bb7c2
leraner23 <killernknwn8@gmail.com>
2025-11-14 00:00:44 +0545

    Add local Spleeter project files

---
+++ src/main/java/com/example/AISpleeter/AiSpleeterApplication.java
@@ -0,0 +1,15 @@
+package com.example.AISpleeter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiSpleeterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiSpleeterApplication.class, args);
+		System.out.println("Ai Voice and instrument spliteer");
+
+	}
+
+}
---
+++ src/test/java/com/example/AISpleeter/AiSpleeterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AISpleeter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiSpleeterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
